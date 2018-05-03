861f0856e84f2deb7a828b1b02d2c247e4e605d5
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-05-03 21:36:01 +0100

    new url for appium

--- mobile_testing/appium_cucumber/features/support/env.rb
+++ mobile_testing/appium_cucumber/features/support/env.rb
@@ -9,7 +9,7 @@ def opts
     caps: {
       platformName: "Android",
       deviceName: "emulator-5554",
-      app: "/Users/tech-a11/Engr7/mobile_testing/budgetwatch.apk"
+      app: "/Users/tech-a11/Engr7/sparta_mobile_testing/mobile_testing/budgetwatch.apk"
     }
   }
 end
